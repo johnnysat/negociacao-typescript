d9f93d7f60336ea920a81b8b8c07e31286e78860
Johnny <johnny_s.o@hotmail.com>
2023-02-13 18:51:51 -0300

    Adicionado método função método criarNegociacao

--- app/controllers/negociacao-controllers.ts
+++ app/controllers/negociacao-controllers.ts
@@ -12,11 +12,16 @@ export class NegociacaoController {
   }
 
   adiciona(){
+    const negociacao = this.criarNegociacao();
+    console.log(negociacao)
+  }
+
+  criarNegociacao(){
     const exp = /-/g;
     const date = new Date(this.inputData.value.replace(exp, ','));
     const quantidade = parseInt(this.inputQuantidade.value);
     const valor = parseFloat(this.inputValor.value)
-    const negociacao = new Negociacao(date, quantidade, valor);
-    console.log(negociacao)
+    return new Negociacao(date, quantidade, valor);
   }
+
 }
