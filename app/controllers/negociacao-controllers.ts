2358cb0b231a61218d4ef32d60b6903e1905cf6d
Johnny <johnny_s.o@hotmail.com>
2023-02-18 19:16:48 -0300

    Adicionado dados da negociacao na lista

--- app/controllers/negociacao-controllers.ts
+++ app/controllers/negociacao-controllers.ts
@@ -5,7 +5,7 @@ export class NegociacaoController {
   private inputData: HTMLInputElement;
   private inputQuantidade: HTMLInputElement;
   private inputValor: HTMLInputElement;
-  private negociacoes: Negociacoes;
+  private negociacoes = new Negociacoes();
 
   constructor() {
     this.inputData = document.querySelector('#data');
@@ -15,7 +15,8 @@ export class NegociacaoController {
 
   adiciona(): void{
     const negociacao = this.criarNegociacao();
-    console.log(negociacao);
+    this.negociacoes.adiciona(negociacao);
+    console.log(this.negociacoes.lista());
     this.limparFormulario();
 ;  }
 
