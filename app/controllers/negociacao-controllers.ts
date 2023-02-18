120b75c74de265cfa1724e3856fdb3a4a23af48e
Johnny <johnny_s.o@hotmail.com>
2023-02-18 19:12:59 -0300

    Importante Negociacoes dentro do negociacao-controller

--- app/controllers/negociacao-controllers.ts
+++ app/controllers/negociacao-controllers.ts
@@ -1,9 +1,11 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacaoController {
   private inputData: HTMLInputElement;
   private inputQuantidade: HTMLInputElement;
   private inputValor: HTMLInputElement;
+  private negociacoes: Negociacoes;
 
   constructor() {
     this.inputData = document.querySelector('#data');
